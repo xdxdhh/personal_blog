b7be65ca9d54d805d586b55a45ed8a1641e70847
diana_v <69389455+xdxdhh@users.noreply.github.com>
2024-09-28 21:36:41 +0200

    added workflow (#1)
    added workflow ci/cd

--- _plugins/get_assets_url.rb
+++ _plugins/get_assets_url.rb
@@ -1,6 +1,23 @@
+require 'net/http'
+require 'uri'
+
+def check_exist(url)
+  uri = URI.parse(url)
+  request = Net::HTTP.new(uri.host, uri.port)
+  request.use_ssl = (uri.scheme == "https")
+  
+  response = request.request_head(uri.path) # Only request the header, not the full content
+  
+  # Return true if the response is 200 OK
+  # assert response.code.to_i == 200
+  raise StandardError.new(url) unless response.code.to_i == 200
+  return url
+end
+
+
 module Jekyll
     CONTENT_URL = "https://assets.diana.lumias.cz"
-  
+
     class ProjectsUrlTag < Liquid::Tag
       def initialize(tag_name, name, tokens)
         super
@@ -8,7 +25,7 @@ module Jekyll
       end
   
       def render(context)
-        "#{CONTENT_URL}/projects/#{@name}"
+        check_exist("#{CONTENT_URL}/projects/#{@name}")
       end
     end
   
@@ -19,7 +36,7 @@ module Jekyll
       end
   
       def render(context)
-        "#{CONTENT_URL}/journeys/#{@name}"
+        check_exist("#{CONTENT_URL}/journeys/#{@name}")
       end
     end
   
@@ -30,7 +47,7 @@ module Jekyll
       end
   
       def render(context)
-        "#{CONTENT_URL}/images/#{@name}"
+        check_exist("#{CONTENT_URL}/images/#{@name}")
       end
     end
 
